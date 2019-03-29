65217d8634f8052f5af59929c1c694eae14e1c58
Zac Gordon <email@zacgordon.com>
2019-03-28 22:50:26 -0400

    Updating manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,10 +11,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#363A45',
+        theme_color: '#363A45',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/badge--react-dark.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
